8d41b78013080036f341c7a0e4c80d52d9fd7ff7
Victoria Tran <ptran.vicki@gmail.com>
2015-11-25 01:00:42 -0800

    Edit pseudocode to be more clear & precise

--- FirstFactorial.js
+++ FirstFactorial.js
@@ -1,4 +1,4 @@
-/*Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18. 
+/*Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18.
 
 Use the Parameter Testing feature in the box below to test your code with different arguments.*/
 
@@ -6,14 +6,14 @@ Use the Parameter Testing feature in the box below to test your code with differ
 
 // create firstFactorial function that takes in one param(num)
 var firstFactorial = function(num){
-	// create an empty array called values
+	// create an empty array called "values"
 	var values = [];
 
-	// use for loop with variable i equal to num; as long as i is less than or equal to num, i will increment by 1
+	// use for loop with variable "i" equal to 1; as long as "i" is less than or equal to num, "i" will increment by 1
 	for(var i = 1; i <= num; i++){
-		// push each current value of i into the values array
+		// push each current value of "i" into the "values" array
 		values.push(i);
 	}
-	// return eval invoked on the joined values array with "*"
+	// return eval invoked on the joined "values" array with "*"
 	return eval(values.join('*'));
 };
