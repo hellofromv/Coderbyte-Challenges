2166f3f28a34381daeaa407dcafcfac9d35c7237
Damon <damonnguyen92@gmail.com>
2015-11-24 19:54:28 -0800

    Commit FirstReverse instruction

---
+++ FirstReverse.js
@@ -0,0 +1,3 @@
+/*Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+Use the Parameter Testing feature in the box below to test your code with different arguments.*/
